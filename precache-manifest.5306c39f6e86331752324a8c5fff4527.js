a9c7788901bb0feca9aaed46e535758e2c8f6897
bhuvi <50789213+bhuvanesh15@users.noreply.github.com>
2020-08-26 00:31:37 +0530

    Updates

---
+++ precache-manifest.5306c39f6e86331752324a8c5fff4527.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "375776012d86d9ba6def728ec85d252c",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "eb56d03e959ef7b5db36",
+    "url": "/portfolio/static/css/main.725cf990.chunk.css"
+  },
+  {
+    "revision": "16920b54a15e9fc80566",
+    "url": "/portfolio/static/js/2.9696ffc6.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/portfolio/static/js/2.9696ffc6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb56d03e959ef7b5db36",
+    "url": "/portfolio/static/js/main.c05deed3.chunk.js"
+  },
+  {
+    "revision": "711327842eda866ac99b",
+    "url": "/portfolio/static/js/runtime-main.58762675.js"
+  },
+  {
+    "revision": "daeaf731dec30f7b361b6cffbbbc307f",
+    "url": "/portfolio/static/media/avatar2.daeaf731.png"
+  }
+]);
